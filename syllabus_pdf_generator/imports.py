429df55b81f017e433b748c4148d5f8fea95941d
DhruthiV <dhruthivenkatesh.pmcs.369@gmail.com>
2025-03-14 12:05:05 +0530

    all imports here

---
+++ syllabus_pdf_generator/imports.py
@@ -0,0 +1,9 @@
+# syllabus_pdf_generator/imports.py
+
+# ReportLab imports
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+# IO Handling
+from io import BytesIO
